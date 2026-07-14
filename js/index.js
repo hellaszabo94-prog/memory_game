b51e70183bd95a69d15572c73c1162f71e38ab43
Hella Szabo <szabo.hella94@gmail.com>
2026-07-14 17:03:55 +0200

    create the array for the deck

---
+++ js/index.js
@@ -0,0 +1,27 @@
+/* Array for the pictures */
+const picturesAboutPlaces=[
+    "egypt.jpg",
+    "egypt.jpg",
+    "hungary.jpg",
+    "hungary.jpg",
+    "india.jpg",
+    "india.jpg",
+    "italy.jpg",
+    "italy.jpg",
+    "malaysia.jpg",
+    "malaysia.jpg",
+    "united_kingdom.jpg",
+    "united_kingdom.jpg",
+    "usa.jpg",
+    "usa.jpg",
+    "vietnam.jpg",
+    "vietnam.jpg"
+];
+
+console.log("I have " + picturesAboutPlaces.length + " card in the deck." );
+
+/* Array for the pictures */
+
+/* Function for mix the cards*/
+
+/* Function for mix the cards*/
