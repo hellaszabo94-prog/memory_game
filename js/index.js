e0c48c0920383d80cf81120f0d45e88f944bce8c
Hella Szabo <szabo.hella94@gmail.com>
2026-07-15 18:21:00 +0200

    add a function to create the new html elements

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-/* Array for the pictures */
+/* array for the pictures */
+
 const picturesAboutPlaces=[
     "egypt.jpg",
     "egypt.jpg",
@@ -20,12 +21,12 @@ const picturesAboutPlaces=[
 
 console.log("I have " + picturesAboutPlaces.length + " card in the deck." );
 
-/* Array for the pictures */
+/* array for the pictures */
 
-/* Function for mix the cards*/
+/* function for mix the cards */
 
 function mixTheDeck (picturesAboutPlaces){
-    /*a for loop for the circle*/
+    /* a for loop for the circle */
     for (let i = picturesAboutPlaces.length - 1; i > 0 ; i--){
         /*get a random number between 0 and 15*/
         const randomCard = Math.floor(Math.random() * (i + 1));
@@ -36,9 +37,47 @@ function mixTheDeck (picturesAboutPlaces){
     }
     return picturesAboutPlaces;
 }
-/*call the function*/
+/* call the function */
 let gameDeck = mixTheDeck(picturesAboutPlaces);
 
 console.log("The mixed deck:", gameDeck );
 
-/* Function for mix the cards*/
+/* function for mix the cards */
+
+/* add cards to the html*/
+
+/* choose the element where I would place the new elements */
+const gamePlace = document.querySelector("#gamePlace");
+
+/* functions to add the new cards */
+
+function addTheGameBoard () {
+
+    /* clean the gameplace */
+    gamePlace.innerHTML = "";
+
+    /* add every card from the deck */
+    gameDeck.forEach(function(pics) {
+
+        /* create a variabel for the new html element */
+        const cardHtml= `
+                <div class="card">
+                    <div class="backSide">
+                        <div class="circle right"></div>
+                        <div class="circle left"></div>
+                        <div class="circle under"></div>
+                    </div>
+                    <div class="frontSide">
+                        <img src="${pics}">
+                    </div>
+                </div>`;
+        
+        /* add the new html element to the game place */
+        gamePlace.innerHTML+=cardHtml;
+
+    });
+
+}
+/* call the function*/
+addTheGameBoard();
+/* add cards to the html*/
